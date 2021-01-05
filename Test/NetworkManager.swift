166ce18de1649b138aec99bf629190d631ae7989
Simran Singh Sandhu <simran@Simrans-MacBook-Air.local>
2021-01-05 21:34:10 +0530

    API Content Type

--- Test/NetworkManager.swift
+++ Test/NetworkManager.swift
@@ -13,52 +13,16 @@ class NetworkManager {
     
     func postLoginCredentials(email: String, password: String, completion: @escaping(Result<LoginModal, Error>) -> ()) {
 
-        //reactnative@test.com
-        //Test@1234
-        let parameters = [
-          [
-            "key": "email",
-            "value": email,
-            "type": "text"
-          ],
-          [
-            "key": "password",
-            "value": password,
-            "type": "text"
-          ]] as [[String : Any]]
-        
-        let boundary = "Boundary-\(UUID().uuidString)"
-        var body = ""
-//        let error: Error? = nil
-        for param in parameters {
-          if param["disabled"] == nil {
-            let paramName = param["key"]!
-            body += "--\(boundary)\r\n"
-            body += "Content-Disposition:form-data; name=\"\(paramName)\""
-            if param["contentType"] != nil {
-              body += "\r\nContent-Type: \(param["contentType"] as! String)"
-            }
-            let paramType = param["type"] as! String
-            if paramType == "text" {
-              let paramValue = param["value"] as! String
-              body += "\r\n\r\n\(paramValue)\r\n"
-            } else {
-              let paramSrc = param["src"] as! String
-              let fileData = try! NSData(contentsOfFile:paramSrc, options:[]) as Data
-              let fileContent = String(data: fileData, encoding: .utf8)!
-              body += "; filename=\"\(paramSrc)\"\r\n"
-                + "Content-Type: \"content-type header\"\r\n\r\n\(fileContent)\r\n"
-            }
-          }
-        }
-        body += "--\(boundary)--\r\n";
-        let postData = body.data(using: .utf8)
+        let parameters = ["email": email, "password": password]
         
         var request = URLRequest(url: URL(string: "https://empti.org/empti/public/api/login")!,timeoutInterval: Double.infinity)
-        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
         
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+                return
+        }
         request.httpMethod = "POST"
-        request.httpBody = postData
+        request.httpBody = httpBody
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
           guard let data = data else {
@@ -87,46 +51,24 @@ class NetworkManager {
     
     func getShopDetails(token: String, completion: @escaping(Result<ShopModal, Error>) -> ()) {
         
-        let parameters = [
-          [
-            "key": "outlet_id",
-            "value": "86",
-            "type": "text"
-          ]] as [[String : Any]]
-        
-        let boundary = "Boundary-\(UUID().uuidString)"
-        var body = ""
-//        var error: Error? = nil
-        for param in parameters {
-          if param["disabled"] == nil {
-            let paramName = param["key"]!
-            body += "--\(boundary)\r\n"
-            body += "Content-Disposition:form-data; name=\"\(paramName)\""
-            if param["contentType"] != nil {
-              body += "\r\nContent-Type: \(param["contentType"] as! String)"
-            }
-            let paramType = param["type"] as! String
-            if paramType == "text" {
-              let paramValue = param["value"] as! String
-              body += "\r\n\r\n\(paramValue)\r\n"
-            } else {
-              let paramSrc = param["src"] as! String
-              let fileData = try! NSData(contentsOfFile:paramSrc, options:[]) as Data
-              let fileContent = String(data: fileData, encoding: .utf8)!
-              body += "; filename=\"\(paramSrc)\"\r\n"
-                + "Content-Type: \"content-type header\"\r\n\r\n\(fileContent)\r\n"
-            }
-          }
-        }
-        body += "--\(boundary)--\r\n";
-        let postData = body.data(using: .utf8)
-
+//        let parameters = [
+//          [
+//            "key": "outlet_id",
+//            "value": "86",
+//            "type": "text"
+//          ]
+//
+        let parameters = ["outlet_id": "86"]
+       
         var request = URLRequest(url: URL(string: "https://empti.org/empti/public/api/user/outletDetail")!,timeoutInterval: Double.infinity)
         request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
 
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+                return
+        }
         request.httpMethod = "POST"
-        request.httpBody = postData
+        request.httpBody = httpBody
 
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
           guard let data = data else {
